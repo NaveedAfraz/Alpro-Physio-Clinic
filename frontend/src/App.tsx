1c06ab6ac007f1ae36d5cb8a115529c918fff323
NaveedAfraz <naveedafraz123@gmail.com>
2025-09-12 23:53:03 +0530

    added stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router";
 import { CuppingCoursePage } from "./components/page2/cupping";
 import { MedicalTourismPage } from "./components/page3/tourism";
 import ScrollToTop from "./components/scrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
